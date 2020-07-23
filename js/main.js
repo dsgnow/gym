e6e55a6c40b2d55f3db29e1baa53d746fc7d127b
workStation <ptr.stachowicz@gmail.com>
2020-07-23 15:13:28 +0200

    offer section rwd and swipe on mobile

--- js/main.js
+++ js/main.js
@@ -40,3 +40,122 @@ $('.navBurger__btn').on('click', function (e) {
     $hambPop.toggleClass('navBurger--hide');
     $hambPop.show();
 })
+
+// swipe event //
+class Swipe {
+    constructor(element) {
+        this.xDown = null;
+        this.yDown = null;
+        this.element = typeof (element) === 'string' ? document.querySelector(element) : element;
+
+        this.element.addEventListener('touchstart', function (evt) {
+            this.xDown = evt.touches[0].clientX;
+            this.yDown = evt.touches[0].clientY;
+        }.bind(this), false);
+
+    }
+
+    onLeft(callback) {
+        this.onLeft = callback;
+
+        return this;
+    }
+
+    onRight(callback) {
+        this.onRight = callback;
+
+        return this;
+    }
+
+    onUp(callback) {
+        this.onUp = callback;
+
+        return this;
+    }
+
+    onDown(callback) {
+        this.onDown = callback;
+
+        return this;
+    }
+
+    handleTouchMove(evt) {
+        if (!this.xDown || !this.yDown) {
+            return;
+        }
+
+        let xUp = evt.touches[0].clientX;
+        let yUp = evt.touches[0].clientY;
+
+        this.xDiff = this.xDown - xUp;
+        this.yDiff = this.yDown - yUp;
+
+        if (Math.abs(this.xDiff) > Math.abs(this.yDiff)) { // Most significant.
+            if (this.xDiff > 0) {
+                this.onLeft();
+            } else {
+                this.onRight();
+            }
+        } else {
+            if (this.yDiff > 0) {
+                this.onUp();
+            } else {
+                this.onDown();
+            }
+        }
+
+        // Reset values.
+        this.xDown = null;
+        this.yDown = null;
+    }
+
+    run() {
+        this.element.addEventListener('touchmove', function (evt) {
+            this.handleTouchMove(evt);
+        }.bind(this), false);
+    }
+}
+
+const swiper = new Swipe('.columnOfferPlans__wrapOffer');
+let offers = [...document.querySelectorAll('.columnOfferPlans__offer')];
+
+const clearSwipe = function () {
+    offers.forEach(function (offer, index) {
+        if (offer.classList.contains('columnOfferPlans__offer--active')) {
+            planIndex = index;
+        }
+        offers[planIndex].classList.remove('columnOfferPlans__offer--active');
+    });
+
+    document.querySelector('.columnOfferPlans__wrapOffer').classList.toggle('columnOfferPlans__wrapOffer--active');
+}
+
+swiper.onLeft(function () {
+
+    clearSwipe();
+
+    if (planIndex !== offers.length - 1) {
+        offers[planIndex + 1].classList.add('columnOfferPlans__offer--active');
+        planIndex++;
+    } else if (planIndex == offers.length - 1) {
+        offers[0].classList.add('columnOfferPlans__offer--active');
+        planIndex++;
+    }
+
+});
+
+swiper.onRight(function () {
+
+    clearSwipe();
+
+    if (planIndex !== 0) {
+        offers[planIndex - 1].classList.add('columnOfferPlans__offer--active');
+        planIndex--;
+    } else if (planIndex == 0) {
+        offers[2].classList.add('columnOfferPlans__offer--active');
+        planIndex == 2;
+    }
+
+});
+
+swiper.run();
